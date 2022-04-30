5b008feee67ee5674a1602352a8e374f209dac3e
Fuktorius <fuktorius@hotmail.com>
2022-04-30 14:09:03 +0200

    First Commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World from SFML"<<"\n";
+	system("Pause");
+	return 0;
+}
